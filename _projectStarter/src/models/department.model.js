dba725ea730c41f8589efef5785d4b96a406cf4e
muleykecevik <muleyke.cevik@gmail.com>
2024-03-23 22:28:05 +0300

    23/3/24

---
+++ _projectStarter/src/models/department.model.js
@@ -0,0 +1,18 @@
+"use strict"
+const { Schema } = require('mongoose')
+/* -------------------------------------------------------
+    EXPRESS - Personnel API
+------------------------------------------------------- */
+
+const { mongoose } = require('../configs/dbConnection')
+
+const DepartmentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+    }
+}, { collection: "departments", timestamps: true })
+module.exports = mongoose.model('Department', DepartmentSchema)
+
